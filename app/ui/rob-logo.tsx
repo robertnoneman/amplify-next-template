787e85e7753a2b512adbd1e57cfb019a07faa14c
Robert Noneman <robert.noneman@ngc.com>
2025-01-03 14:15:46 -0500

    fix logo on mobile again

--- app/ui/rob-logo.tsx
+++ app/ui/rob-logo.tsx
@@ -6,9 +6,9 @@ export default function RobLogo() {
     <div
       className={`${roboto.className} flex flex-row items-center leading-none`}
     >
-      <FaceSmileIcon className="h-10 w-10 md:h-12 md:w-12 rotate-[-15deg]" />
-      <p className="text-[44px] "><strong>ROBDAY</strong></p>
-      <FaceSmileIcon className="h-10 w-10 md:h-12 md:w-12 rotate-[15deg]" />
+      <FaceSmileIcon className="h-6 w-6 md:h-12 md:w-12 rotate-[-15deg]" />
+      <p className="text-[28px] md:text-[44px] "><strong>ROBDAY</strong></p>
+      <FaceSmileIcon className="h-6 w-6 md:h-12 md:w-12 rotate-[15deg]" />
     </div>
   );
 }
