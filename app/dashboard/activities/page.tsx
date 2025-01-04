5c729e68d6072ad42a513ccdd97f364888e9bcb0
Robert Noneman <robert.noneman@ngc.com>
2025-01-04 18:47:34 -0500

    playing with activity list theme

--- app/dashboard/activities/page.tsx
+++ app/dashboard/activities/page.tsx
@@ -7,6 +7,7 @@ import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import styles from '@/app/dashboard/activities/styles.module.css';
+import { Fade, Logo, Button, StyleOverlay, IconButton, Row, Text, Input, Column, Flex } from "@/once-ui/components";
 
 Amplify.configure(outputs);
 
@@ -28,31 +29,123 @@ export default function Page() {
     function createTodo() {
       client.models.Todo.create({
         content: window.prompt("Todo content"),
+        isDone: false
       });
     }
 
-    function deleteTodo(id: string) {
-      client.models.Todo.delete({ id })
+    function deleteTodo(id: string, content: any, isDone: any) {
+      // client.models.Todo.delete({ id })
+      const todo = {
+        id: id,
+        isDone: !isDone,
+        content: content
+      }
+      client.models.Todo.update(todo);
+      // client.models.Todo.delete({ id });
+      // client.models.Todo.create({ ...todo });
+      // client.models.Todo.update.arguments = { id: id, isDone: true, content: content }
     }
     
   return (
     <main>
+      <Column fillWidth paddingY="80" paddingX="xs" alignItems="center" flex={1}>
+      <Fade
+        zIndex={3}
+        pattern={{
+          display: true,
+          size: "4",
+        }}
+        position="fixed"
+        top="0"
+        left="0"
+        to="bottom"
+        height={5}
+        fillWidth
+        blur={0.25}
+      />
+      <Row position="fixed" top="0" fillWidth justifyContent="center" zIndex={3}>
+        <Row
+          data-border="rounded"
+          justifyContent="space-between"
+          maxWidth="l"
+          paddingRight="64"
+          paddingLeft="32"
+          paddingY="20"
+        >
+          <Logo size="m" icon={false} href="https://itsrobday.com" />
+          <Row gap="12" hide="s">
+            <Button
+              href="https://github.com/robertnoneman/amplify-next-template"
+              prefixIcon="github"
+              size="s"
+              label="GitHub"
+              weight="default"
+              variant="tertiary"
+            />
+            <StyleOverlay top="20" right="24" />
+          </Row>
+          <Row gap="16" show="s" alignItems="center" paddingRight="24">
+            <IconButton
+              href="https://github.com/robertnoneman/amplify-next-template"
+              icon="github"
+              variant="tertiary"
+            />
+            <StyleOverlay top="20" right="24" />
+          </Row>
+        </Row>
+      </Row>
       <h1 className={`${roboto.className} text-white mb-4 text-xl md:text-2xl text-center`}>
           Activities page
       </h1>
       <h2 className={`${roboto.className} text-xl text-gray-50 md:text-3xl md:leading-normal`}>Robday Activity List</h2>
       <button className={`${styles.button}`} onClick={createTodo}>+ new</button>
+      
       <ul className={`${styles.ul}`}>
+        <div className={`${styles.li} ${roboto.className} `}>
+          <li>
+            <Flex background="surface" fillWidth >
+              <Column alignItems="left" paddingTop="4" fillWidth gap="0">
+                <Row fillWidth justifyContent="space-around">
+                  <Text variant="body-default-xl" align="left"> 
+                    Activity
+                  </Text>
+                  <Text variant="body-default-xl" align="left"> 
+                    Status
+                  </Text>
+                </Row>
+              </Column>
+            </Flex>
+          </li>
+        </div>
         {todos.map((todo) => (
-          <div className={`${styles.li} ${roboto.className} text-white`}>
+          <div className={`${styles.li} ${roboto.className} ${todo.isDone} `}>
             <li
-              onClick={() => deleteTodo(todo.id)}
+              onClick={() => deleteTodo(todo.id, todo.content, todo.isDone)}
               key={todo.id}>
-                {todo.content}
+                <Row fillWidth gap="-1" alignItems="center" justifyContent="space-between">
+                  <Input 
+                    id="content"
+                    label={`${todo.content}`}
+                    labelAsPlaceholder
+                    radius="left"
+                    defaultValue={`${todo.content}`}
+                  />
+                  <Input
+                    id="isDone"
+                    label="Status"
+                    labelAsPlaceholder
+                    radius="right"
+                    defaultValue={`${todo.isDone ? "Done" : "Not Done"}`}
+                  />
+                  {/* <Text variant="body-default-m"> 
+                    {todo.isDone ? "Done" : "Not Done"}
+                  </Text> */}
+                </Row>
             </li>
           </div>
         ))}
       </ul>
+      </Column>
     </main>
   )
 }
--- app/ui/dashboard/topnav.tsx
+++ app/ui/dashboard/topnav.tsx
@@ -46,10 +46,11 @@ export default function TopNav() {
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
                 {navigation.map((item) => (
-                  <a
+                  <DisclosureButton
                     key={item.name}
+                    as='a'
                     href={item.href}
-                    // aria-current={item.current ? 'page' : undefined}
+                    aria-current={item.current ? 'page' : undefined}
                     // className={classNames(
                     //   item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                     //   'rounded-md px-3 py-2 text-sm font-medium',
@@ -64,7 +65,7 @@ export default function TopNav() {
                     )}
                   >
                     {item.name}
-                  </a>
+                  </DisclosureButton>
                 ))}
               </div>
             </div>
