6f55365b3b9352f77336cbc34de434809ce9b7ee
Robert Noneman <robert.noneman@ngc.com>
2025-01-13 09:19:06 -0500

    I don't even care how it looks on desktop

--- app/page.tsx
+++ app/page.tsx
@@ -56,191 +56,62 @@ export default function Page() {
 
   return (
     <Column fillWidth paddingY="0" paddingX="0" alignItems="center" flex={1}>
-      {/* <Confetti
-      width={width}
-      height={height}
-      recycle={false}
-      // numberOfPieces={1000}
-      tweenDuration={10000}
-      /> */}
       <Column
         overflow="hidden"
         as="main"
         // maxWidth="l"
         position="relative"
         // radius="xl"
-        alignItems="flex-start"
+        // alignItems="flex-start"
         // border="neutral-alpha-weak"
         fillWidth
+        fillHeight
       >
-      
-    {/* <main className="flex min-h-screen flex-col p-0 bg-gray-800 bg-opacity-90"> */}
         <Row height={5} fillWidth background="accent-strong" alignItems="flex-end" justifyContent='space-between'>
-        {/* <div className="flex h-20 shrink-0 items-end rounded-lg bg-[#bb4444] text-white md:bg-opacity-90 p-4 justify-between md:h-52"> */}
             <RobLogo />
-        {/* </div> */}
         </Row>
-        {/* <Row position="absolute" background='accent-medium' fillWidth height={800} style={{zIndex: -20}}/> */}
-        
-          {/* <Image
-                src={robdaycropblur}
-                // width={1500}
-                // height={160}
-                sizes="100vh"
-                // style={{
-                //     objectFit: "contain",
-                //     zIndex: -1,
-                //     position: "fixed",
-                //     }}
-                // fill={false}
-                alt="The Robs"
-                // style={{transform: translate(0, 20)}}
-                // className="hidden md:block"
-                className="absolute inset-0 -z-10 size-full object-contain object-top md:object-top translate-y-20 opacity-20"
-            /> */}
-        {/* </Row> */}
-        {/* <SmartImage 
-          src="/robdaycropblur.jpeg"
-          objectFit='cover'
-          position='absolute'
-          height={30}
-          paddingRight="xl"
-          // aspectRatio='16:9'
-          style={{opacity: 50, objectFit: 'fill', height: '100vh', transform: 'translateX(-20%)'}}
-        /> */}
-        {/* <div className=""> */}
-            {/* <Image
-                src={robdaycropblur}
-                // width={1500}
-                // height={160}
-                sizes="100vh"
-                // style={{
-                //     objectFit: "contain",
-                //     zIndex: -1,
-                //     position: "fixed",
-                //     }}
-                // fill={false}
-                alt="The Robs"
-                // className="hidden md:block"
-                className="absolute inset-0 -z-10 size-full object-cover object-top md:object-top"
-            /> */}
-            {/* <div className="mt-4 flex grow flex-col gap-4 md:flex-row z-20"> */}
-            <Column fillHeight justifyContent='center' fillWidth minHeight={5}>
-                {/* <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 bg-opacity-80 px-6 py-10 md:w-2/5 md:px-20"> */}
-                {/* <RevealFx delay={0.6} fillWidth> */}
-                {/* <Column height={5} background="surface" justifyContent='center' radius="xs" paddingX="4" paddingY="12" gap="0" alignItems="left" border="surface"/> */}
-                <Column height={50} background="surface" justifyContent='flex-end' paddingX="0" paddingY="0" gap="0" alignItems="left">
-                    {/* <p
-                        className={`${roboto.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
-                    > */}
-                      <SmartImage 
-                        src="/robdaycrop.jpeg"
-                        objectFit='cover'
-                        // sizes="100vw"
-                        position="absolute"
-                        // height={30}
-                        zIndex={-1}
-                        // height={20}
-                        // width={25}
-                        fillWidth
-                        opacity={10}
-                        background='transparent'
-                        // style={{maskImage: 'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.5) 50%, rgba(0,0,0,0) 100%)'}}
-                        // radius="s-8"
-                        // paddingRight="xl"
-                        // aspectRatio='16:9'
-                        // style={{opacity: 50, objectFit: 'fill', height: '100vh', transform: 'translateX(-20%)'}}
+        <Column 
+          fillHeight 
+          justifyContent='flex-end' 
+          // fillWidth 
+          // minHeight={5}
+        >
+          <Column 
+            // height={50} 
+            background="surface" 
+            justifyContent='flex-end' 
+            paddingX="0" paddingY="0" gap="0" alignItems="left"
+          >
+            <Column paddingX="12" paddingY="12">
+              <Text variant="display-default-m" onSolid="brand-strong">
+                Happy Robday!<br></br><br/>
+              </Text>
+              <Text>
+                When it's not Robday, try <br/>
+              </Text>
+              <SmartLink href="https://itsnotrobday.com" style={{color: "#0070f3", margin: 0}}>
+                <b>itsnotrobday.com</b>
+              </SmartLink>
+                <br></br>
+                <Button
+                  id="trigger"
+                  variant="primary"
+                  size="l"
+                  href="/dashboard"
+                  style={{backgroundColor: "#0070f3"}}
+                >
+                  <Flex fillHeight justifyContent='center' alignItems='center'>
+                    Explore
+                    <Arrow
+                      trigger="#trigger"
+                      color="onBackground"
                       />
-                      <Column paddingX="12" paddingY="12">
-                        <Text variant="display-default-m" onSolid="brand-strong">
-                          Happy Robday!<br></br><br/>
-                        </Text>
-                          <Text>
-                          When it's not Robday, try <br/>
-                          </Text>
-                          <SmartLink href="https://itsnotrobday.com" style={{color: "#0070f3", margin: 0}}>
-                          <b>itsnotrobday.com</b>
-                          </SmartLink>
-                        {/* <a href="https://itsnotrobday.com" className="text-blue-500">
-                            itsnotrobday.com
-                            </a> */}
-                    {/* </p> */}
-                    {/* <Link
-                        href="/dashboard"
-                        className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-                    >
-                        <span>Explore</span> <ArrowRightIcon className="w-5 md:w-6" />
-                        </Link> */}
-                    <br></br>
-                        <Button
-                          id="trigger"
-                          variant="primary"
-                          size="l"
-                          href="/dashboard"
-                          style={{backgroundColor: "#0070f3"}}
-                        >
-                          <Flex fillHeight>
-                            Explore
-                            <Arrow
-                              trigger="#trigger"
-                              color="onBackground"
-                              />
-                          </Flex>
-                        </Button>
-                    </Column>
-                </Column>
-                {/* </RevealFx> */}
-                {/* </div> */}
-                {/* <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12"> */}
-                    {/* Add Hero Images Here */}
-                  {/* <Column fillHeight justifyContent='center' width="s"> */}
-                    {/* <SmartImage 
-                      src="/robdaycropblur.jpeg"
-                      objectFit='cover'
-                      sizes="100vw"
-                      // position='absolute'
-                      // height={30}
-                      height={20}
-                      // paddingRight="xl"
-                      // aspectRatio='16:9'
-                      // style={{opacity: 50, objectFit: 'fill', height: '100vh', transform: 'translateX(-20%)'}}
-                    /> */}
-                    {/* <Image
-                        // src={robdaycropblur}
-                        src="/robday.jpeg"
-                        width={1500}
-                        height={1600}
-                        // sizes="100vh"
-                        // style={{
-                            //     objectFit: "cover",
-                        //     zIndex: -1,
-                        //     position: "fixed",
-                        //   }}s
-                        fill={false}
-                        alt="Screenshots of the dashboard project showing desktop version"
-                        className="hidden md:block"
-                        /> */}
-                        {/* <Image
-                        src="/S+RGroupPics-122.jpg"
-                        width={560}
-                        height={620}
-                        alt="Screenshot of the dashboard project showing mobile version"
-                        className="block md:hidden"
-                        /> */}
-                    {/* </Column> */}
-                {/* </div> */}
-            {/* </div> */}
+                  </Flex>
+                </Button>
             </Column>
-            <Fade>
-
-            </Fade>
-            <Column fillHeight justifyContent='center' width="s"></Column>
-        {/* </div> */}
-        {/* <Flex fillWidth border="neutral-medium" background="page">
-        
-        </Flex> */}
-    {/* </main> */}
-      <Pride autorun={{ speed: 60, duration: 5000 }} />
+          </Column>
+        </Column>
+        <Pride autorun={{ speed: 60, duration: 5000 }} />
       </Column>
       <Dialog
           isOpen={!isRobDay()}
